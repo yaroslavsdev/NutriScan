b9a170c09bfda097d74285cc7fa653acabf32958
yaroslavsdev <yaroslav.safronov.09@yandex.ru>
2025-12-26 12:21:12 +0300

    Поддержка тёмной темы

--- NutriScanApp/app/src/main/java/com/yaroslavsdev/nutriscan/MainActivity.kt
+++ NutriScanApp/app/src/main/java/com/yaroslavsdev/nutriscan/MainActivity.kt
@@ -5,7 +5,14 @@ import com.yaroslavsdev.nutriscan.ui.screens.*
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.rememberNavController
 import com.yaroslavsdev.nutriscan.ui.theme.NutriScanTheme
@@ -13,13 +20,22 @@ import com.yaroslavsdev.nutriscan.ui.theme.NutriScanTheme
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
         setContent {
             NutriScanTheme {
-                AppNavigation()
+                Scaffold(
+                    modifier = Modifier.fillMaxSize()
+                ) { innerPadding ->
+                    AppNavigation(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
             }
         }
     }
 }
+
 
 
 
--- NutriScanApp/app/src/main/java/com/yaroslavsdev/nutriscan/NavHost.kt
+++ NutriScanApp/app/src/main/java/com/yaroslavsdev/nutriscan/NavHost.kt
@@ -1,16 +1,18 @@
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.yaroslavsdev.nutriscan.ui.screens.*
 
 @Composable
-fun AppNavigation() {
+fun AppNavigation(modifier: Modifier) {
     val navController = rememberNavController()
 
     NavHost(
         navController = navController,
-        startDestination = "home"
+        startDestination = "home",
+        modifier = modifier
     ) {
         composable("home") { HomeScreen(navController) }
         composable("scanner") { ScannerScreen(navController) }
