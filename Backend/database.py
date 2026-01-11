7ba06915be81dc69010a66e0ae4fb18f016369ad
yaroslavsdev <yaroslav.safronov.09@yandex.ru>
2026-01-11 14:21:53 +0300

    Добавлен docker-compose для БД и сервера

--- Backend/database.py
+++ Backend/database.py
@@ -3,11 +3,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-# Настройки из переменных окружения (которые в Docker)
-# Формат: postgresql://user:password@hostname:port/dbname
-# DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://user:password@localhost:5432/nutriscan")
-
-DATABASE_URL = "postgresql://postgres:Pingvin2009@localhost:5432/nutriscan"
+DATABASE_URL = "postgresql://postgres:password@db:5432/nutriscan"
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
